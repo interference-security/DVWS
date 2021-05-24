8e7641b24dccb03ded8095cce792d0ba27ec48d3
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2021-05-24 15:00:18 -0400

    Added heartbeat command and updated formatting

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ $page_data = <<<EOT
 <div class="row">
     <div class="col-md-12">
         <p>
-            OWASP Damn Vulnerable Web Sockets (DVWS) is a vulnerable web application which works on web sockets for client-server communication. The flow of the application is similar to <a href="https://github.com/ethicalhack3r/DVWA" target="_blank">DVWA</a>.<br><br>
+            <a href="https://owasp.org/www-project-damn-vulnerable-web-sockets/">OWASP Damn Vulnerable Web Sockets (DVWS)</a> is a vulnerable web application which works on web sockets for client-server communication. The flow of the application is similar to <a href="https://github.com/ethicalhack3r/DVWA" target="_blank">DVWA</a>.<br><br>
             In the "hosts" file of your attacker machine create an entry for "dvws.local" to point at the IP address hosting the DVWS application.
-Location of the "hosts" file:<br>
+Location of the "hosts" file:<br><br>
 Windows: C:\windows\System32\drivers\etc\hosts<br>
-Linux: /etc/hosts<br>
+Linux: /etc/hosts<br><br>
 Sample entry for hosts file:<br>
 192.168.100.199&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dvws.local<br><br>
             The application requires the following:
@@ -21,12 +21,14 @@ Sample entry for hosts file:<br>
                 <li><a href="https://github.com/bixuehujin/reactphp-mysql/" target="_blank">ReactPHP-MySQL</a></li>
             </ol><br>
             Set the MySQL hostname, username, password and an existing database name in the "<u><i>includes/connect-db.php</i></u>" file then go to <a href="setup.php">Setup</a> to finish setting up DVWS.<br><br>
-            On the host running this application, run the following command from DVWS directory: <b>php ws-socket.php</b><br><br>
+            On the host running this application, run the following command from DVWS directory:<br>
+            <br><pre>composer install
+php ws-socket.php --heartbeat-interval 10</pre><br>
             This open-source project is hosted here <a href="https://github.com/interference-security/DVWS/" target="_blank">https://github.com/interference-security/DVWS/</a>.<br><br>
             DVWS created by <a href="https://twitter.com/xploresec">@xploresec</a><br><br>
             <a href="https://twitter.com/xploresec"><img src="img/twitter.png" style="width:48px; height:48px;"></a> &nbsp;&nbsp;&nbsp; 
             <a href="https://github.com/interference-security/DVWS/"><img src="img/github.png" style="width:48px; height:48px;"></a> &nbsp;&nbsp;&nbsp; 
-            <a href="https://www.owasp.org/index.php/OWASP_Damn_Vulnerable_Web_Sockets_(DVWS)"><img src="img/owasp.png" style="width:48px; height:48px;"></a>
+            <a href="https://owasp.org/www-project-damn-vulnerable-web-sockets/"><img src="img/owasp.png" style="width:48px; height:48px;"></a>
 
         </p>
     </div>
